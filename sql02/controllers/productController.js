8b1922a46c367bb3a40ff13ae07a5ea9e6d54291
pakinporing <pakinporing@gmail.com>
2023-01-13 17:26:23 +0700

    labbbbb

--- sql02/controllers/productController.js
+++ sql02/controllers/productController.js
@@ -1,25 +1,45 @@
-const product = require('../models/product');
+const Product = require('../models/Product');
 
 exports.getAllProducts = (req, res, next) => {
-  product.findAll().then((rows) => {
+  Product.findAll().then((rows) => {
     res.json(rows);
   });
 };
 exports.getProduct = (req, res, next) => {
   const { id } = req.params;
-  product.findByid(id).then((rows) => {
+  Product.findById(id).then((rows) => {
     res.json(rows);
   });
 };
 exports.createProduct = (req, res, next) => {
   const product = req.body;
+  Product.create(product)
+    .then((rs) => {
+      if (rs.affectedRows) return res.json({ id: rs.insertId, ...product });
+      // {id: rs.insertId, name: product.name,... }
+      throw new Error('Cannot create this data');
+    })
+    .catch(next);
 };
 
 exports.updateProduct = (req, res, next) => {
   const { id } = req.params;
   const product = req.body;
+  Product.update(id, product)
+    .then((rs) => {
+      if (rs.affectedRows) return res.json({ id, ...product });
+      throw new Error('Cannot update this data');
+    })
+    .catch(next);
 };
 
 exports.deleteProduct = (req, res, next) => {
   const { id } = req.params;
+  Product.delete(id)
+    .then((rs) => {
+      if (rs.affectedRows)
+        return res.json({ msg: `id: ${id} have been deleted` });
+      throw new Error('cannot delete this id');
+    })
+    .catch(next);
 };
--- sql02/models/product.js
+++ sql02/models/product.js
@@ -1,11 +1,28 @@
 const db = require('../utils/db');
 
 exports.findAll = () => {
-  let sql = 'select * from products';
+  let sql = 'Select * from products';
   return db.execute(sql).then(([rs]) => rs);
 };
 
-exports.findByid = (id) => {
-  let sql = 'select * from products where id=?';
+exports.findById = (id) => {
+  let sql = 'Select * from products Where id=?';
+  return db.execute(sql, [id]).then(([rs]) => rs);
+};
+
+exports.create = (product) => {
+  const { name, price, cat_id } = product;
+  let sql = 'Insert into products (name, price, cat_id) Values (?, ?, ?)';
+  return db.execute(sql, [name, price, cat_id]).then(([rs]) => rs);
+};
+
+exports.update = (id, product) => {
+  const { name, price, cat_id } = product;
+  let sql = 'Update products Set name=?, price=?, cat_id=? Where id=?';
+  return db.execute(sql, [name, price, cat_id, id]).then(([rs]) => rs);
+};
+
+exports.delete = (id) => {
+  let sql = 'Delete from products where id=?';
   return db.execute(sql, [id]).then(([rs]) => rs);
 };
